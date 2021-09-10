2fdd9841af7223c6b31833e11b862ec612ce4015
zvizvi <5633910@gmail.com>
2021-09-10 17:15:34 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {
+    semi: ['error', 'always'],
+    'arrow-parens': ['warn', 'always'],
+    'object-shorthand': ['warn', 'always'],
+    'space-before-function-paren': ['off'],
+    'vue/max-attributes-per-line': ['off'],
+    'vue/html-closing-bracket-newline': ['off'],
+    'vue/html-indent': ['off'],
+    'vue/html-self-closing': ['warn', {
+      html: {
+        void: 'always',
+        normal: 'never',
+        component: 'always'
+      },
+      svg: 'always',
+      math: 'always'
+    }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+};
